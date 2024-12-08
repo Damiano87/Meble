70f8ddea4068db733061a8e23b7bda67e05082a7
Damian Sobek <sobe66@gmail.com>
2024-12-08 23:12:15 +0100

    Added more info

--- api/seed.ts
+++ api/seed.ts
@@ -578,4 +578,325 @@ const products = [
       "1 X Szafka RTW ATLANTA jasne drewno, karton 2/2 (25.7 kg) 140.00 x 16.50 x 50.00 cm (dł. x szer. x wys.)",
     ],
   },
+  {
+    name: "Blender chromowany",
+    price: 32500,
+    category: "kuchnia",
+    likes: 123,
+    title: "Blender 50's Retro Style BLF03RDEU Czerwony",
+    description:
+      "Przedstawiamy niezwykły blender 50's Retro Style BLF03RDEU od marki Smeg. Ten blender to połączenie wyjątkowego stylu retro z nowoczesną technologią, który doskonale wpisze się w charakter Twojej kuchni.",
+    images: [
+      "https://res.cloudinary.com/damiano/image/upload/v1733423554/blender1_a7pone.webp",
+      "https://res.cloudinary.com/damiano/image/upload/v1733423555/blender2_bunmfv.webp",
+      "https://res.cloudinary.com/damiano/image/upload/v1733423556/blender3_brm7u5.webp",
+    ],
+    stock: 28,
+    features: [
+      "Smoothie - dla kremowych koktajli",
+      "Smoothie warzywne - specjalnie zaprojektowane do koktajli z zielonych warzyw",
+      "Rozdrabnianie lodu do koktajli i lodowych napojów",
+      "AutoClean gwarantuje szybkie i dokładne czyszczenie; Wystarczy wlać trochę wody i niewielką ilość neutralnego detergentu, aby aktywować funkcję, a Twój blender będzie znowu gotowy do działania",
+      "Smooth Start  umożliwia stopniowe zwiększanie prędkości podczas uruchamiania urządzenia, aż do osiągnięcia żądanej prędkości",
+      "Pulse  posiada trzy poziomy intensywności, umożliwia niestandardowe przetwarzanie przygotowawcze",
+      "Cztery poziomy prędkości",
+    ],
+    offers: ["1 x Blender"],
+    details: [
+      { name: "Numer artykułu", description: "283070-178140" },
+      { name: "Gwarancja", description: "24 miesiące" },
+      { name: "Rodzaj", description: "Blender" },
+      { name: "Kolor", description: "Czerwony" },
+      { name: "Materiał", description: "Plastik" },
+      {
+        name: "Materiał dodatkowy",
+        description: "Metal",
+      },
+      { name: "Waga", description: "4 kg" },
+      { name: "Montaż", description: "Nie wymaga montażu" },
+    ],
+    dimensions: [
+      { name: "Wysokość", dim: "40 cm" },
+      { name: "Średnica", dim: "20 cm" },
+    ],
+  },
+  {
+    name: "Krzesło do jadalni",
+    price: 27900,
+    category: "kuchnia",
+    likes: 80,
+    title: "Nowoczesne krzesło bez podłokietników",
+    description:
+      "Te nowoczesne krzesła do jadalni charakteryzują się eleganckim i minimalistycznym designem oraz brakiem podłokietników, co umożliwia ustawienie ich blisko stołu. Ich wyróżniającą cechą jest wdzięcznie zakrzywione oparcie, które zapewnia zarówno wygodę, jak i elegancki wizualny akcent w jadalni. Minimalistyczny styl krzesła podkreślają solidne, metalowe nogi, które zapewniają stabilność. Umieszczone wokół stołu będą nie tylko funkcjonalnym miejscem do siedzenia, ale także stylowym elementem wystroju.",
+    images: [
+      "https://res.cloudinary.com/damiano/image/upload/v1733423557/chair1_njoy5i.webp",
+      "https://res.cloudinary.com/damiano/image/upload/v1733423558/chair2_xoojye.webp",
+      "https://res.cloudinary.com/damiano/image/upload/v1733423559/chair3_fexzt5.webp",
+    ],
+    stock: 15,
+    features: [
+      "Dopasowany kształt dla najwyższego komfortu",
+      "Doskonale łączy się z wieloma stylami wnętrz",
+      "Wytrzymała i solidna konstrukcja ramy",
+      "Wytrzymała tapicerka",
+      "Nakładki na nóżki chronią podłogę przed zarysowaniem",
+    ],
+    offers: ["2 x Krzesło"],
+    details: [
+      { name: "Numer artykułu", description: "307931-409059" },
+      { name: "Gwarancja", description: "24 miesiące" },
+      { name: "Rodzaj", description: "Zestaw 2 krzeseł do jadalni" },
+      { name: "Kolor", description: "Beżowy" },
+      { name: "Odcień", description: "Kremowy" },
+      { name: "Materiał", description: "Poliester" },
+      {
+        name: "Materiał dodatkowy",
+        description: "Żelazo, Sklejka, Drewno topolowe",
+      },
+      { name: "Wykończenie", description: "Malowany proszkowo" },
+      { name: "Waga", description: "5 kg" },
+      { name: "Maks. obciążenie", description: "120 kg" },
+      { name: "Montaż", description: "Częściowo zmontowany" },
+      { name: "Sztaplowane", description: "Nie" },
+    ],
+    dimensions: [
+      { name: "Wysokość", dim: "50 cm" },
+      { name: "Głębokość", dim: "59 cm" },
+      { name: "Szerokość", dim: "81 cm" },
+      { name: "Powierzchnia siedziska", dim: "50 x 47 cm" },
+      { name: "Wysokość siedziska", dim: "45 cm" },
+    ],
+    additionalInfo:
+      "By zagwarantować stabilność konstrukcji i bezpieczeństwo podczas korzystania z produktu, zaleca się sprawdzenie czy montaż przebiegł poprawnie po 2 tygodniach od pierwszego złożenia. Dodatkowo zalecamy sprawdzanie, czy śruby są dokręcone co 3 miesiące.",
+    materialDetails: [
+      { name: "Skład materiału", description: ["100% Poliester"] },
+      { name: "Gatunek drewna", description: ["Populus"] },
+      { name: "Pochodzenie drewna", description: ["Chiny"] },
+    ],
+    careTips: [
+      {
+        tip: "Poliester",
+        list: [
+          "Plamy należy usuwać za pomocą środka do czyszczenia tapicerki i wilgotnej gąbki lub łagodnego detergentu.",
+          "Odkurzać wyłącznie przy pomocy odpowiedniej końcówki.",
+        ],
+      },
+      {
+        tip: "Żelazo",
+        list: [
+          "Czyścić za pomocą ogólnodostępnych, delikatnych środków myjących oraz miękkiej ściereczki, po przetarciu na mokro wytrzeć do sucha.",
+          "Aby uniknąć zarysowań, nie używać mleczka do czyszczenia.",
+          "Elementy miedziane lub chromowane należy polerować przeznaczonymi do tego środkami.",
+        ],
+      },
+    ],
+    packing: [
+      "1 X Zestaw 2 krzeseł, KIANA, kremowy (11.5 kg) 72.00 x 50.00 x 53.00 cm (dł. x szer. x wys.)",
+    ],
+  },
+  {
+    name: "Stół do jadalni rozkładany 150/190 x 90 cm jasne drewno MADOX",
+    price: 151900,
+    category: "kuchnia",
+    likes: 200,
+    title: "Rozsuwany stół w stylu retro o naturalnym wykończeniu",
+    description:
+      "Ten stół rozkładany w kolorze jasnego drewna, inspirowany stylem skandynawskim stanie się ponadczasowym elementem każdej jadalni. Wytrzymały blat został wykonany z wysokiej jakości płyty MDF, która jest łatwa w pielęgnacji. Smukłe nogi z drewna kauczukowego dopełniają elegancki wygląd stołu, gwarantując jego stabilność. Ponadczasowe wzornictwo sprawia, że stół sprawdzi się w różnych aranżacjach wnętrz, zarówno tradycyjnych, jak i nowoczesnych.",
+    images: [
+      "https://res.cloudinary.com/damiano/image/upload/v1733423557/chair1_njoy5i.webp",
+      "https://res.cloudinary.com/damiano/image/upload/v1733423558/chair2_xoojye.webp",
+      "https://res.cloudinary.com/damiano/image/upload/v1733423559/chair3_fexzt5.webp",
+    ],
+    stock: 15,
+    features: [
+      "W stylistyce retro",
+      "Doskonale łączy się z wieloma stylami wnętrz",
+      "Elegancki, ciepły kolor",
+      "Łatwe w pielęgnacji i wytrzymałe materiały",
+      "Rozkładany blat stołu zapewnia dodatkowe miejsce",
+    ],
+    offers: ["1 x Stół"],
+    details: [
+      { name: "Numer artykułu", description: "292322-389471" },
+      { name: "Gwarancja", description: "24 miesiące" },
+      { name: "Rodzaj", description: "Stół do jadalni" },
+      { name: "Kolor", description: "Jasne drewno" },
+      { name: "Materiał", description: "MDF" },
+      {
+        name: "Materiał dodatkowy",
+        description: "Drewno kauczukowe",
+      },
+      { name: "Wykończenie", description: "Fornirowany, Lakierowany" },
+      { name: "Waga", description: "32 kg" },
+      { name: "Maks. obciążenie", description: "80 kg" },
+      { name: "Mechanizm rozsuwania", description: "System butterfly" },
+      { name: "Montaż", description: "Częściowo zmontowany" },
+      { name: "Kształt", description: "Prostokątny" },
+      { name: "Miejsce dla", description: "6 osób" },
+    ],
+    dimensions: [
+      { name: "Wysokość", dim: "75 cm" },
+      { name: "Szerokość", dim: "90 cm" },
+      { name: "Długość", dim: "150 / 190 cm" },
+      { name: "Wysokość nóżek", dim: "64 cm" },
+    ],
+    additionalInfo:
+      "Aby zachować trwałość i estetykę blatu, zalecamy stosowanie podkładek pod naczynia oraz jak najszybsze usuwanie mokrych plam i zabrudzeń, co pomoże uniknąć uszkodzeń produktu.",
+    materialDetails: [
+      { name: "Gatunek drewna", description: ["Hevea brasiliensis"] },
+      { name: "Pochodzenie drewna", description: ["Malezja"] },
+    ],
+    careTips: [
+      {
+        tip: "MDF",
+        list: [
+          "Używać wyłącznie łagodnych środków czystości oraz miękkiej ściereczki. Po przetarciu na mokro dokładnie wytrzeć do sucha.",
+        ],
+      },
+      {
+        tip: "Drewno kauczukowe",
+        list: [
+          "Do czyszczenia należy używać wyłącznie łagodnych detergentów oraz miękkiej ściereczki, wycierając powierzchnię w kierunku słojów.",
+          "Aby chronić mebel przed wilgocią, po przetarciu na mokro należy go dokładnie wytrzeć do sucha.",
+          "Dla produktów drewnianych z olejowanym wykończeniem: można ponownie olejować po dłuższym użytkowaniu, by utrzymać jego dobry wygląd.",
+        ],
+      },
+    ],
+    packing: [
+      "1 X Stół do jadalni rozkładany, MADOX jasne drewno 150-190x90x75 cm (36.0 kg) 156.00 x 13.00 x 94.00 cm (dł. x szer. x wys.)",
+    ],
+  },
+  {
+    name: "Regał metalowy srebrny HOXIE",
+    price: 154900,
+    category: "salon",
+    likes: 87,
+    title: "Regał w stylu glamour",
+    description:
+      "Jeśli szukasz dodatkowej przestrzeni do eksponowania swoich ulubionych książek, bibelotów czy pamiątek, ten ozdobny regał będzie idealnym wyborem. Został wyposażony w 4 szerokie półki wykonane ze szkła hartowanego i oparty na ramie ze stali nierdzewnej w błyszczącym srebrnym kolorze, która gwarantuje jego trwałość. Umieść go w pustym rogu salonu, domowego biura lub sypialni i obserwuj, jak staje się najbardziej przyciągającym wzrok meblem we wnętrzu.",
+    images: [
+      "https://res.cloudinary.com/damiano/image/upload/v1733423622/regal1_nj0ayt.webp",
+      "https://res.cloudinary.com/damiano/image/upload/v1733423623/regal2_gpkanh.webp",
+      "https://res.cloudinary.com/damiano/image/upload/v1733423624/regal3_ocvcp2.webp",
+    ],
+    stock: 4,
+    features: [
+      "Praktyczne półki do przechowywania drobiazgów",
+      "Elegancki, wyjątkowy element wnętrza",
+      "Nowoczesny design",
+      "Precyzyjne wykończenie",
+      "Stabilna i wytrzymała konstrukcja",
+    ],
+    offers: ["1 x Regał"],
+    details: [
+      { name: "Numer artykułu", description: "271689-179220" },
+      { name: "Gwarancja", description: "24 miesiące" },
+      { name: "Rodzaj", description: "Regał" },
+      { name: "Kolor", description: "Srebrny" },
+      { name: "Materiał", description: "Stal nierdzewna" },
+      {
+        name: "Materiał dodatkowy",
+        description: "Szkło hartowane",
+      },
+      { name: "Wykończenie", description: "Błyszczący" },
+      { name: "Waga", description: "28 kg" },
+      { name: "Maks. obciążenie półki", description: "4 kg" },
+      { name: "Maks. obciążenie góry", description: "4 kg" },
+      { name: "Liczba półek", description: "4" },
+      { name: "Montaż", description: "Wymaga kompletnego montażu" },
+    ],
+    dimensions: [
+      { name: "Wysokość", dim: "175 cm" },
+      { name: "Szerokość", dim: "100 cm" },
+      { name: "Głębokość", dim: "30 cm" },
+    ],
+    careTips: [
+      {
+        tip: "Stal nierdzewna",
+        list: [
+          "Czyścić za pomocą ogólnodostępnych, delikatnych środków myjących oraz miękkiej ściereczki, po przetarciu na mokro wytrzeć do sucha.",
+          "Aby uniknąć zarysowań, nie używać mleczka do czyszczenia.",
+          "Elementy miedziane lub chromowane należy polerować przeznaczonymi do tego środkami.",
+        ],
+      },
+      {
+        tip: "Szkło hartowane",
+        list: [
+          "Do czyszczenia używać wyłącznie łagodnych środków czystości lub detergentów do szkła oraz miękkiej ściereczki. Po przetarciu na mokro, wytrzeć do sucha.",
+          "Aby uniknąć zarysowań, nie używać mleczka do czyszczenia.",
+        ],
+      },
+    ],
+    packing: [
+      "1 X Regał, HOXIE, srebrny 1/2 (16.6 kg) 107.00 x 12.00 x 34.00 cm (dł. x szer. x wys.)",
+      "1 X Regał, HOXIE, srebrny 2/2 (17.6 kg) 183.00 x 12.00 x 108.00 cm (dł. x szer. x wys.)",
+    ],
+  },
+  {
+    name: "Regał 3 półki ciemne drewno KEENE",
+    price: 53900,
+    category: "salon",
+    likes: 50,
+    title: "Drewniany regał z szufladą",
+    description:
+      "Odśwież wnętrze swojego mieszkania dzięki temu stylowemu regałowi. Cztery szerokie półki sprawdzą się do eksponowania zdjęć, książek czy ustawienia kwiatów, a pojemna szuflada oferuje dodatkowe miejsce na przechowanie. Wykonany z wytrzymałych materiałów, regał nie wymaga częstej pielęgnacji, by służyć przez wiele lat. Jego kolory pozwalają na dopasowanie go do pomieszczeń w stylu retro, czy w nowoczesnym, niezależnie od ich wystroju.",
+    images: [
+      "https://res.cloudinary.com/damiano/image/upload/v1733423617/drugiregal1_syo21c.webp",
+      "https://res.cloudinary.com/damiano/image/upload/v1733423617/drugiregal2_mno3ct.webp",
+      "https://res.cloudinary.com/damiano/image/upload/v1733423618/drugiregal3_gsaysn.webp",
+    ],
+    stock: 4,
+    features: [
+      "Połączenie piękna z funkcjonalnością",
+      "Wykończenie imitujące drewno dla naturalnego akcentu",
+      "Najwyższej jakości materiały",
+      "Wytrzymała i solidna konstrukcja",
+      "Wiele miejsca do przechowywania",
+    ],
+    offers: ["1 x Regał"],
+    details: [
+      { name: "Numer artykułu", description: "305860-179814" },
+      { name: "Gwarancja", description: "24 miesiące" },
+      { name: "Rodzaj", description: "Regał" },
+      { name: "Kolor", description: "Ciemne drewno, Czarny" },
+      { name: "Materiał", description: "Płyta pilśniowa" },
+      {
+        name: "Materiał dodatkowy",
+        description: "MDF, Żelazo",
+      },
+      { name: "Wykończenie", description: "Matowy, Imitacja drewna" },
+      { name: "Waga", description: "24 kg" },
+      { name: "Maks. obciążenie półki", description: "10 kg" },
+      { name: "Maks. obciążenie góry", description: "15 kg" },
+      { name: "Maks. obciążenie szuflady", description: "5 kg" },
+      { name: "Liczba półek", description: "3" },
+      { name: "Montaż", description: "Wymaga kompletnego montażu" },
+    ],
+    dimensions: [
+      { name: "Wysokość", dim: "101 cm" },
+      { name: "Szerokość", dim: "56 cm" },
+      { name: "Głębokość", dim: "51 cm" },
+    ],
+    additionalInfo:
+      "Aby zachować trwałość i estetykę blatu, zalecamy stosowanie podkładek pod naczynia oraz jak najszybsze usuwanie mokrych plam i zabrudzeń, co pomoże uniknąć uszkodzeń produktu.",
+    careTips: [
+      {
+        tip: "Płyta pilśniowa",
+        list: [
+          "Używać wyłącznie łagodnych środków czystości oraz miękkiej ściereczki. Po przetarciu na mokro dokładnie wytrzeć do sucha.",
+        ],
+      },
+      {
+        tip: "Żelazo",
+        list: [
+          "Czyścić za pomocą ogólnodostępnych, delikatnych środków myjących oraz miękkiej ściereczki, po przetarciu na mokro wytrzeć do sucha.",
+          "Aby uniknąć zarysowań, nie używać mleczka do czyszczenia.",
+          "Elementy miedziane lub chromowane należy polerować przeznaczonymi do tego środkami.",
+        ],
+      },
+    ],
+    packing: [
+      "1 X Regał, KEENE, ciemne drewno, karton 1/1 (24.3 kg) 112.00 x 16.00 x 62.00 cm (dł. x szer. x wys.)",
+    ],
+  },
 ];
