b07d504d1689dd70b0226f1518041e92bb64bc08
Damian Sobek <sobe66@gmail.com>
2025-04-07 11:34:00 +0200

    Ready to deploy

--- api/dist/routes/root.js
+++ api/dist/routes/root.js
@@ -2,9 +2,8 @@ import express from "express";
 const router = express.Router();
 import path from "path";
 import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-router.get("^/$|/index(.html)?", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "API działa poprawnie" });
 });
 export default router;
--- api/dist/server.js
+++ api/dist/server.js
@@ -22,17 +22,18 @@ dotenv.config();
 console.log(process.env.NODE_ENV);
 const app = express();
 if (process.env.NODE_ENV === "production") {
-    app.use(morgan("combined"));
-}
-else {
-    app.use(morgan("dev"));
+  app.use(morgan("combined"));
+} else {
+  app.use(morgan("dev"));
 }
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json());
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use("/", express.static(path.join(__dirname, "/public")));
+
 app.use("/", root);
 app.use("/users", userRoutes);
 app.use("/products", productsRoutes);
@@ -42,29 +43,17 @@ app.use("/cart", cartRoutes);
 app.use("/wishlist", wishListRoutes);
 app.use("/stripe", stripeRoutes);
 app.use("/orders", orderRoutes);
-app.all("*", (req, res) => {
-    res.status(404);
-    if (req.accepts("html")) {
-        res.sendFile(path.join(__dirname, "views", "404.html"));
-    }
-    else if (req.accepts("json")) {
-        res.json({ message: "404 Not Found" });
-    }
-    else {
-        res.type("txt").send("404 Not Found");
-    }
-});
+
 app.use(errorHandler);
 const checkDatabaseConnection = async () => {
-    try {
-        await prisma.$connect();
-        console.log("Connected to MongoDB");
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    }
-    catch (error) {
-        console.error("Failed to connect MongoDB:", error);
-    }
+  try {
+    await prisma.$connect();
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect MongoDB:", error);
+  }
 };
 checkDatabaseConnection();
